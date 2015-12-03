b25ae3920c0cd3bb761126eaf920ebe00abcf30c
azb <azbaney@gmail.com>
2015-12-02 23:00:39 -0800

    Merged the Masses

--- UberForDronesControlApp/src/uberfordronescontrolapp/UberForDronesControlApp.java
+++ UberForDronesControlApp/src/uberfordronescontrolapp/UberForDronesControlApp.java
@@ -15,6 +15,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ListIterator;
+import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
@@ -29,6 +30,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
@@ -42,14 +44,43 @@ import javax.swing.UIManager;
 public class UberForDronesControlApp extends JApplet {
     
     private static final int JFXPANEL_WIDTH_INT = 1400;
-    private static final int JFXPANEL_HEIGHT_INT = 600;
+    private static final int JFXPANEL_HEIGHT_INT = 800;
     private static JFXPanel fxContainer;
     private fakeTime time = new fakeTime(0, 0, 0);
 
+    private static double[] dronex = new double[100]; 
+    private static double[] droney = new double[100];
+    
+    private static double[] drone_target_x = new double[100];
+    private static double[] drone_target_y = new double[100];
+    
+    private static double[] drone_start_x = new double[100];
+    private static double[] drone_start_y = new double[100];
+    
+    private static String[] drone_status = new String[100];
+    private static double[] drone_battery_level = new double[100];
+    
+    private static ArrayList<Label> DroneStatusLabels = new ArrayList<Label>();
+    private static ArrayList<Label> DroneBatteryLabels = new ArrayList<Label>();
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+            
+    for(int i = 0 ; i < 10 ; i++)
+        {
+        dronex[i] = Math.random() * 600;
+        droney[i] = Math.random() * 400;
+        drone_start_x[i] = Math.random() * 600;
+        drone_start_y[i] = Math.random() * 400;
+        drone_target_x[i] = Math.random() * 600;
+        drone_target_y[i] = Math.random() * 400;
+        drone_status[i] = "Flying";
+        drone_battery_level[i] = 100 - Math.random() * 25;
+        }
+    
        try {
             // The newInstance() call is a work around for some
             // broken Java implementations
@@ -110,7 +141,7 @@ public class UberForDronesControlApp extends JApplet {
           conn =
 	               DriverManager.getConnection(
 	            		           "jdbc:mysql://localhost:3306/test?" +
-	                               "user=root&password=MYSQL"
+	                               "user=root&password="
 	            		           );
           stmt = conn.createStatement();
           // Drop off drones/packages and update drivers
@@ -290,44 +321,103 @@ public class UberForDronesControlApp extends JApplet {
     
     private void createScene() {
         
+        VBox overlord = new VBox();
         HBox dronesTable = new HBox();
         GridPane dronesTableGrid = new GridPane();
+        dronesTableGrid.setMinWidth(700);
+        overlord.getChildren().add(dronesTable);
         
         Canvas canvas = new Canvas(600,600);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
         
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
         
         Image image1 = new Image("/theMap.png", 800, 800, true, false);
         Image mapMarker = new Image("/mapMarker.png", 800, 800, true, false);
         
         gc.drawImage(image1, 0,0);
         
-        double[] drone_target_x = new double[10];
-        double[] drone_target_y = new double[10];
-        
         for(int i = 0 ; i < 10 ; i++)
         {
-        drone_target_x[i] = Math.random() * 600;
-        drone_target_y[i] = Math.random() * 400;
-        }
-        
-        for(int i = 0 ; i < 10 ; i++)
-        {
-        double dronex = Math.random() * 600;
-        double droney = Math.random() * 400;
+        //double dronex = Math.random() * 600;
+        //double droney = Math.random() * 400;
         
         gc.setFill(Color.WHITE);
-        gc.fillRect(dronex-4,droney+32,50,20);
+        gc.fillRect(dronex[i]-4-16,droney[i]+32-32,50,20);
         gc.setFill(Color.BLACK);
-        gc.fillText("Drone "+(i+1),dronex,droney+48);
-        gc.drawImage(mapMarker, dronex,droney,32,32);
+        gc.fillText("Drone "+(i+1),dronex[i]-16,droney[i]+48-32);
+        gc.drawImage(mapMarker, dronex[i] - 16,droney[i] - 32,32,32);
         gc.setFill(Color.BLUE);
-        gc.strokeLine(dronex + 16, droney + 32, drone_target_x[i], drone_target_y[i]);
+        gc.strokeLine(dronex[i], droney[i], drone_target_x[i], drone_target_y[i]);
+        
+        if (pointDistance(dronex[i],droney[i],drone_target_x[i],drone_target_y[i]) > 2)
+            {
+            double dir = pointDirection(dronex[i],droney[i],drone_target_x[i],drone_target_y[i]);
+        
+            dronex[i] += lengthdir_x(.5,dir);
+            droney[i] += lengthdir_y(.5,dir);
+            
+            if (pointDistance(dronex[i],droney[i],drone_target_x[i],drone_target_y[i]) < 15)
+                {
+                if (drone_target_x[i] != drone_start_x[i] && drone_target_y[i] != drone_start_y[i])
+                drone_status[i] = "Dropping off package";
+                }
+            else
+                {
+                if (drone_target_x[i] == drone_start_x[i] && drone_target_y[i] == drone_start_y[i])
+                    drone_status[i] = "Flying home";
+                else
+                    drone_status[i] = "Flying to destination";
+                }
+            drone_battery_level[i]-=.02;
+            }
+        else
+            {
+            if (drone_target_x[i] == drone_start_x[i] && drone_target_y[i] == drone_start_y[i])
+                {
+                    //If arrived at package processing facility
+                    
+                    if (drone_battery_level[i] < 100)
+                        {
+                        drone_status[i] = "Swapping battery";
+                        drone_battery_level[i]+=.04;
+                        }
+                    else
+                        {
+                        drone_status[i] = "Waiting for ride";
+                        drone_target_x[i] = Math.random() * 600;
+                        drone_target_y[i] = Math.random() * 400;
+                        }
+                    
+                }
+            else
+                {
+                    //If arrived at package drop off destination
+                    
+                    drone_target_x[i] = drone_start_x[i];
+                    drone_target_y[i] = drone_start_y[i];
+                    drone_status[i] = "Dropping off package";
+                }
+            
+            }
+        
+        DroneStatusLabels.get(i+1).setText(drone_status[i]);
+        
+        if (pointDistance(dronex[i],droney[i],drone_start_x[i],drone_start_y[i]) > 4)
+        DroneBatteryLabels.get(i+1).setText("" + (int)drone_battery_level[i] + "%");
+        else
+        DroneBatteryLabels.get(i+1).setText("N/A");
         }
         
         ImageView iv3 = new ImageView();
         iv3.setImage(image1);
-
+        }
+        };
+        
+        timer.start();
+        
         dronesTable.getChildren().add(dronesTableGrid);
         dronesTable.getChildren().add(canvas);
         
@@ -337,11 +427,11 @@ public class UberForDronesControlApp extends JApplet {
         dronesTableGrid.setVgap(20);
                 
         //DRONE IDS
+        Button droneIDButton = new Button();
+        droneIDButton.setText("Drone ID [v]");
         Label DroneIDLabel = new Label();
         DroneIDLabel.setText("Drone ID");
-        dronesTableGrid.add( DroneIDLabel, 0, 0);
-        
-        
+        dronesTableGrid.add( droneIDButton, 0, 0);
         
         Connection conn = null;
 	Statement stmt = null;
@@ -388,36 +478,64 @@ public class UberForDronesControlApp extends JApplet {
 	            System.out.println("VendorError: " + ex.getErrorCode());
         }
         //DESTINATIONS
-        DroneIDLabel = new Label();
-        DroneIDLabel.setText("Destination");
-        dronesTableGrid.add( DroneIDLabel, 1, 0);
+        Button destinationButton = new Button();
+        destinationButton.setText("Destination [v]");
+        Label DestinationLabel = new Label();
+        DestinationLabel.setText("Destination");
+        dronesTableGrid.add( destinationButton, 1, 0);
         
         //DRIVER IDS
-        DroneIDLabel = new Label();
-        DroneIDLabel.setText("Driver ID");
-        dronesTableGrid.add( DroneIDLabel, 2, 0);
+        Button driverIDButton = new Button();
+        driverIDButton.setText("Package ID [v]");
+        Label DriverIDLabel = new Label();
+        DriverIDLabel.setText("Driver ID");
+        dronesTableGrid.add( driverIDButton, 2, 0);
         
         //PACKAGE IDS
+        Button packageIDButton = new Button();
+        packageIDButton.setText("Package ID [v]");
         DroneIDLabel = new Label();
         DroneIDLabel.setText("Package ID");
-        dronesTableGrid.add( DroneIDLabel, 3, 0);
+        dronesTableGrid.add(packageIDButton , 3, 0); //DroneIDLabel
         
         //BATTERY LIFE
-        DroneIDLabel = new Label();
-        DroneIDLabel.setText("Battery Life");
-        dronesTableGrid.add( DroneIDLabel, 4, 0);
+        Button sortButton = new Button();
+        sortButton.setText("Battery [v]");
+        Label DroneBatteryLabel = new Label();
+        DroneBatteryLabel.setText("Battery Life");
+        DroneBatteryLabels.add(DroneBatteryLabel);        
+        dronesTableGrid.add( sortButton, 4, 0); //DroneBatteryLabels.get(0)
         
+        /*
         for(int row = 0 ; row < 10 ; row++)
         {
         DroneIDLabel = new Label();
         DroneIDLabel.setText(""+(row+1)*10+"%");
         dronesTableGrid.add( DroneIDLabel, 4, row+1);
         }
+        */
         
         //DRONE STATUS
-        DroneIDLabel = new Label();
-        DroneIDLabel.setText("Status");
-        dronesTableGrid.add( DroneIDLabel, 5, 0);        
+        Label DroneStatusLabel = new Label();
+        DroneStatusLabel.setText("Status");
+        DroneStatusLabels.add(DroneStatusLabel);
+        dronesTableGrid.add( DroneStatusLabels.get(0), 5, 0);
+        
+        for(int row = 0 ; row < 10 ; row++)
+        {
+        DroneStatusLabel = new Label();
+        DroneStatusLabel.setText(drone_status[row]);
+        DroneStatusLabels.add(DroneStatusLabel);
+        DroneBatteryLabel = new Label();
+        DroneBatteryLabel.setText(""+(int)drone_battery_level[row]+"%");
+        DroneBatteryLabels.add(DroneBatteryLabel);
+        }
+        
+        for(int row = 0 ; row < 10 ; row++)
+        {
+        dronesTableGrid.add( DroneStatusLabels.get(row+1), 5, row+1);
+        dronesTableGrid.add( DroneBatteryLabels.get(row+1), 4, row+1);
+        }
         
         Button btn = new Button();
         btn.setText("Update");
@@ -430,10 +548,37 @@ public class UberForDronesControlApp extends JApplet {
         });
         StackPane root = new StackPane();
         //root.getChildren().add(btn);
-        root.getChildren().add(dronesTable);
-        root.getChildren().add(btn);
+        overlord.getChildren().add(btn);
+        root.getChildren().add(overlord);
+        
         
         fxContainer.setScene(new Scene(root));
     }
-    
+    public double lengthdir_x(double speed, double dir){
+		return Math.cos(dir)*speed;
+	}
+    public double lengthdir_y(double speed, double dir){
+		return -Math.sin(dir)*speed;
+	}
+    public double pointDirection(double x1, double y1, double x2, double y2){
+		double xchange = (x2-x1);
+		double ychange = (y2-y1);
+		double dir;
+		if (xchange == 0){
+			if (ychange >= 0)
+				dir = -Math.PI/2;
+			else
+				dir = Math.PI/2;
+		}
+		else{
+			dir = Math.atan2(xchange,ychange)-Math.PI/2;
+		}
+		return dir;
+	}
+    public double pointDistance(double x1, double y1, double x2, double y2){
+		double xchange = (x2-x1);
+		double ychange = (y2-y1);
+		double output = Math.sqrt((xchange*xchange)+(ychange*ychange));
+		return output;
+	}
 }
